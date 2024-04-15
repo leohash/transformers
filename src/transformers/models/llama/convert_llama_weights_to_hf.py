92e088f461611e4790ce8a25d44cbac80447a3d2
Joshua Lochner <admin@xenova.com>
2024-04-15 17:36:50 +0200

    Update convert_llama_weights_to_hf.py

--- src/transformers/models/llama/convert_llama_weights_to_hf.py
+++ src/transformers/models/llama/convert_llama_weights_to_hf.py
@@ -304,7 +304,7 @@ class Llama3Converter(TikTokenConverter):
         chat_template = (
             "{% set loop_messages = messages %}"
             "{% for message in loop_messages %}"
-            "{% set content = '<|start_header_id|>' + message['role'] + '<|end_header_id|>\n\n'+ message['content'].strip() + '<|eot_id|>' %}"
+            "{% set content = '<|start_header_id|>' + message['role'] + '<|end_header_id|>\n\n'+ message['content'] | trim + '<|eot_id|>' %}"
             "{% if loop.index0 == 0 %}"
             "{% set content = bos_token + content %}"
             "{% endif %}"
